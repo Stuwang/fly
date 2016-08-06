914151c12749c76a6be01779fe7a2f9fbf47fedb
wangyuan <799086345@qq.com>
2016-08-06 10:16:52 +0800

    update

--- fly/base/Endian.h
+++ fly/base/Endian.h
@@ -1,11 +1,24 @@
 #ifndef FLY_ENDIAN_H__
 #define FLY_ENDIAN_H__
 
+#include <algorithm>
+
 namespace fly {
 
 template<typename T>
 inline T NetToHost(T value) {
+	int len = sizeof(T);
+	char *p = reinterpret_cast<char*>(&value);
+	std::reverse(p, p + len);
+	return value;
+};
 
+template<typename T>
+inline T HostToNet(T value) {
+	int len = sizeof(T);
+	char *p = reinterpret_cast<char*>(&value);
+	std::reverse(p, p + len);
+	return value;
 };
 
 }
--- test/main.cpp
+++ test/main.cpp
@@ -1,4 +1,11 @@
-#include <base/Logger.h>
+// #include <base/Logger.h>
+// #include <base/Endian.h>
+
+// #include <arpa/inet.h>
+
+// #include <iostream>
+
+using namespace std;
 
 struct fuck {
 	fuck() {data = 0;};
@@ -6,12 +13,25 @@ struct fuck {
 	int data;
 };
 
-int main() {
-
-	fuck f;
-	f.Get() = 12;
-	const char *a = "aaaaa";
-	const char *b = "bbbbb";
-	int ret = memcmp(a, b, 5);
-	LOG_INFO << -1 << '\n' ;
+void test() {
+	int i = 1;
+	if (*(char*)&i > *( ((char*)&i) + 2) ) {
+		std::cout << "xiaoduan";
+	} else {
+		std::cout << "daduan";
+	}
 };
+
+int main() {
+	int i = fly::NetToHost(fly::HostToNet(1));
+	printf("%08X\n", 1);
+	printf("%08X\n", i);
+	int l = ntohl(1);
+	assert(i == l);
+	test();
+};
+
+#define  err(msg)\
+perror("msg");
+
+err(aaaa);
