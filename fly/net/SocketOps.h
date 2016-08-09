b162638fac0060b3b14a08ea78f380ce4b617ed6
wangyuan <799086345@qq.com>
2016-08-09 19:36:03 +0800

    update

--- fly/net/SocketOps.h
+++ fly/net/SocketOps.h
@@ -35,7 +35,7 @@ int accept(int socketfd, struct sockaddr_in *addr);
 
 ssize_t read(int sockfd, void *buf, size_t cout);
 ssize_t readv(int sockfd, const struct iovec *iov, int iovcnt);
-ssize_t write(int sockfd, void *buf, size_t cout);
+ssize_t write(int sockfd, const void *buf, size_t cout);
 
 void close(int sockfd);
 void shutdownWrite(int sockfd);
