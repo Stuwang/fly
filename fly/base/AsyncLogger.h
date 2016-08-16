7e9f48797ce82ee2dcd2eb5a4e367ed7e76596b0
王渊 <799086345@qq.com>
2016-08-16 11:21:14 +0800

    update

---
+++ fly/base/AsyncLogger.h
@@ -0,0 +1,17 @@
+#ifndef FLY_ASYNCLOGGER_H__
+#define FLY_ASYNCLOGGER_H__
+
+#include <>
+
+namespace fly
+{
+  class AsyncLogger{
+  public:
+    AsyncLogger();
+    ~AsyncLogger();
+  private:
+
+  };
+}
+
+#endif //FLY_ASYNCLOGGER_H__
