1de274d58fede402d86c7e6ded4665d030283c22
wangyuan <799086345@qq.com>
2016-09-08 00:00:23 +0800

    add Connecter

---
+++ fly/net/Connecter.cpp
@@ -0,0 +1,71 @@
+#include <net/Connecter.h>
+
+namespace fly {
+
+Connecter::Connecter(EventLoop* loop)
+	: loop_(loop)
+	, seted_addr_(false)
+	, connecting_(false)
+	, fd_(0)
+{
+
+};
+
+Connecter::Connecter(EventLoop* loop, const sockaddr& addr)
+	: loop_(loop)
+	, addr_(addr)
+	, seted_addr_(true)
+	, connecting_(false)
+	, fd_(0)
+{
+
+};
+
+void Connecter::SetAddr(const sockaddr& addr) {
+	assert(!IsConnecting());
+	addr_ = addr;
+};
+
+void Connecter::Reset() {
+	assert(!IsConnecting());
+	memset(&addr_, 0, sizeof(addr_));
+	seted_addr_ = false;
+	connecting_ = false;
+	fd_ = 0;
+	chan_.reset();
+};
+
+void Connecter::SetConnectedCallBack(const Functor& f) {
+	cb_ = f;
+};
+
+void Connecter::Start() {
+	assert(!IsConnecting());
+	assert(seted_addr_);
+	assert(!chan_);
+	connecting_ = true;
+	fd_ = socketops::creatNoBlockOrDie();
+	socketops::connect(fd_, &addr_);
+	chan_.reset(new Channel(fd_, loop_->getPoller()));
+	chan_->setWriteCallBack(std::bind(&Connecter::HandleWrite,
+	                                  shared_from_this()));
+	chan_->setErrorCallBack(std::bind(&Connecter::HandleError,
+	                                  shared_from_this()));
+	chan_->enableWrite();
+};
+
+void Connecter::Stop() {
+	Reset();
+};
+
+void Connecter::HandleWrite() {
+	chan_->disableWrite();
+	if (cb_) cb_(fd_);
+	Stop();
+};
+
+void Connecter::HandleError() {
+
+};
+
+}
--- fly/net/Connecter.h
+++ fly/net/Connecter.h
@@ -2,11 +2,35 @@
 #define FLY_CONNECTER_H__
 
 #include <base/Types.h>
+#include <net/TcpConnection.h>
 
 namespace fly {
 
-class Connecter : noncopyable {
+class Connecter : noncopyable,
+	public std::enable_shared_from_this<Connecter> {
+public:
+	typedef std::function<void(int fd)> Functor;
 
+	explicit Connecter(EventLoop* loop);
+	Connecter(EventLoop* loop, const sockaddr& addr);
+	void SetAddr(const sockaddr& addr);
+	void Reset();
+	bool IsConnecting()const {return connecting_;};
+	void SetConnectedCallBack(const Functor& f);
+	void Start();
+	void Stop();
+
+private:
+	void HandleWrite();
+	void HandleError();
+
+	EventLoop *loop_;
+	sockaddr addr_;
+	bool seted_addr_;
+	bool connecting_;
+	int fd_;
+	std::unique_ptr<Channel> chan_;
+	Functor cb_;
 };
 
 }
